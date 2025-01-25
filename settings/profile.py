293a2c6161397f63754988669f72d8a92bf3bd39
danylo <danylo@danylos-MacBook-Air.local>
2025-01-25 08:09:00 +0200

    init

---
+++ settings/profile.py
@@ -0,0 +1,120 @@
+from dataclasses import dataclass
+
+from settings.settings import Settings
+
+PROFILES_PATH = 'settings/profiles.ini'
+DEFAULT_NAME = 'default_mqtt_user'
+
+@dataclass
+class Profile:
+    _name: str
+    _host: str | None = None
+    _port: str | None = None
+    settings: Settings = Settings(PROFILES_PATH)
+    is_created : bool = False
+
+    def set_field(self, key, value):
+        """Setting field in instance and in profiles.ini"""
+        if hasattr(self, '_' + key):
+            setattr(self, '_' + key, value)
+        else:
+            raise AttributeError(f"Attribute '{key}' does not exist in the instance.")
+
+        if self.is_created:
+            self.settings.set_with_save(self.name, str(key), str(value))
+
+    def set_settings(self, settings: dict) -> None:
+        for key, value in settings.items():
+            self.set_field(key, value)
+
+    def create(self) -> None:
+        """Creating profile in profiles.ini."""
+        if self.settings.has_section(self.name):
+            raise ValueError(f'Profile with name "{self.name}" already exists')
+
+        self.settings.add_section_with_save(self.name)
+        self.is_created = True
+
+        # TODO: add exception if error -> remove added fields
+        for attr_name in dir(self):
+            attr = getattr(type(self), attr_name, None)
+            if isinstance(attr, property):
+                self.settings.set_with_save(self._name, attr_name, str(getattr(self, attr_name)))
+
+    def delete(self):
+        ...
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        # TODO: add logic deletion section with old name, adding section with new name
+        self.set_field('name', name)
+
+    @property
+    def host(self):
+        return self._host
+
+    @host.setter
+    def host(self, host_address: str):
+        self.set_field('host', host_address)
+
+    @property
+    def port(self):
+        return self._port
+
+    @port.setter
+    def port(self, port: id):
+        self.set_field('port', port)
+
+
+@dataclass
+class CurrentProfileHandler:
+    """Class only to handle current profile"""
+    settings: Settings = Settings(PROFILES_PATH)
+    current_profile_name = settings.get('current_profile', 'current')
+
+    def change_current_profile(self, profile_name):
+        self.settings.set_with_save('current_profile', 'current', profile_name)
+
+
+class ProfileManager:
+    def __init__(self):
+        super().__init__()
+        self._profiles = [] # TODO: make instances from profiles.ini
+        self.current_handler = CurrentProfileHandler()
+
+    def create_profile(self, profile_name, **kwargs):
+        profile = Profile(profile_name)
+        profile.create()
+        profile.set_settings(kwargs)
+        self.switch_profile(profile.name)
+        self._profiles.append(profile)
+
+    def get_profile_by_name(self, profile_name) -> Profile:
+        ...
+
+    def set_profile_settings(self, profile_name, **kwargs):
+        profile = self.get_profile_by_name(profile_name)
+        profile.set_settings(kwargs)
+
+    def switch_profile(self, profile_name):
+        self.current_handler.change_current_profile(profile_name)
+
+    def delete_profile(self, profile_name):
+        # TODO: dont forget delete from current and set some another
+        profile = self.get_profile_by_name(profile_name)
+        profile.delete()
+        self._profiles.remove(profile)
+        ...
+
+    @property
+    def profiles(self):
+        return self._profiles
+
+    @property
+    def current_profile(self):
+        # TODO: instance, not name
+        return self.current_handler.current_profile_name
---
+++ settings/settings.py
@@ -0,0 +1,41 @@
+import configparser
+import os
+
+class Settings:
+    """Universal class to parse .ini files."""
+    def __init__(self, settings_path):
+        self.settings_path = settings_path
+        self.config = configparser.ConfigParser()
+        self.load_settings()
+
+    def load_settings(self):
+        if os.path.exists(self.settings_path):
+            self.config.read(self.settings_path, encoding="utf-8")
+        else:
+            raise ValueError("Settings file not found")
+
+    def save_settings(self):
+        with open(self.settings_path, "w", encoding="utf-8") as file:
+            self.config.write(file)
+
+    def get_section(self, section):
+        if self.config.has_section(section):
+            return dict(self.config.items(section))
+
+    def has_section(self, section):
+        return self.config.has_section(section)
+
+    def add_section_with_save(self, section):
+        self.config.add_section(section)
+        self.save_settings()
+
+    def get(self, section, key, default=None):
+        if self.config.has_section(section) and self.config.has_option(section, key):
+            return self.config.get(section, key)
+        return default
+
+    def set_with_save(self, section, key, value):
+        if not self.config.has_section(section):
+            raise ValueError(f'Section "{section}" does not exist')
+        self.config.set(section, key, str(value))
+        self.save_settings()
