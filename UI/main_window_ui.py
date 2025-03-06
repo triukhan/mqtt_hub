5dc9c826f3a270b801a1ca9f37f4564efe7926a4
Danylo Triukhan <danylotriukhan@gmail.com>
2025-03-06 09:49:45 +0200

    fix/show-edit-button-after-switching-from-default

--- UI/main_window_ui.py
+++ UI/main_window_ui.py
@@ -357,6 +357,7 @@ class MqttHubUi(QWidget):
     def set_profile(self, profile):
         if profile == profile_manager.current_profile:
             return None
+        self.edit_button.show()
         profile_manager.switch_profile(profile.id)
         self.main_tab.set_clipboard_messages()
         self.update_profile_button()
