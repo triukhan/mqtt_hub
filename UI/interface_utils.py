1d1c182ce219afe358b8eb71c5eeab8a3d2a6535
Danylo Triukhan <danylotriukhan@gmail.com>
2025-03-14 17:27:45 +0200

    fix/better-coloring

--- UI/interface_utils.py
+++ UI/interface_utils.py
@@ -357,32 +357,33 @@ class JsonHighlighter(QSyntaxHighlighter):
         self.font.setStyleHint(QFont.TypeWriter)
 
         self.int_format = QTextCharFormat()
-        self.int_format.setForeground(QColor('#838a5a'))
+        self.int_format.setForeground(QColor('#9de67e'))
         self.int_format.setFont(self.font)
 
         self.key_format = QTextCharFormat()
-        self.key_format.setForeground(QColor('#852c2f'))
+        self.key_format.setForeground(QColor('#82c9f1'))
         self.key_format.setFont(self.font)
 
         self.string_format = QTextCharFormat()
-        self.string_format.setForeground(QColor('#3f9e5c'))
+        self.string_format.setForeground(QColor('#e48368'))
         self.string_format.setFont(self.font)
 
         self.bool_format = QTextCharFormat()
-        self.bool_format.setForeground(QColor('#576291'))
+        self.bool_format.setForeground(QColor('#d077d6'))
         self.bool_format.setFont(self.font)
 
-        self.bracket_format = QTextCharFormat()
-        self.bracket_format.setForeground(QColor('#ffffff'))
-        self.bracket_format.setFont(self.font)
+        self.common_format = QTextCharFormat()
+        self.common_format.setForeground(QColor('#c0c0c0'))
+        self.common_format.setFont(self.font)
 
 
         self.rules = [
             (QRegExp(r'"([^"]+)":'), self.key_format),
             (QRegExp(r'\b(true|false)\b'), self.bool_format),
             (QRegExp(r':\s*(\d+)\b(?!")'), self.int_format),
-            (QRegExp(r'[\[\]{}]'), self.bracket_format),
-            (QRegExp(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'), self.bracket_format),
+            (QRegExp(r'[\[\]{}]'), self.common_format),
+            (QRegExp(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'), self.common_format),
+            (QRegExp(r',\s*$'), self.common_format),
         ]
 
 
